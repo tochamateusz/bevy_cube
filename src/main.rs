d148a89c0a879be7bb8f90a1ffa761cf5efc6493
Tocha Mateusz <tocha.mateusz+main@gmail.com>
2024-01-20 20:03:29 +0100

    feat: Add resource

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,9 @@ impl Plugin for HelloPlugin {
         let fns = (update_people, greet_people);
         let chained_fn = fns.chain();
 
-        app.add_systems(Startup, add_people)
-            .add_systems(Update, (hello_world, chained_fn));
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, chained_fn);
     }
 }
 
@@ -24,9 +25,14 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Test2".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
@@ -37,10 +43,6 @@ fn update_people(mut query: Query<&mut Name, With<Person>>) {
             break;
         }
     }
-}
-
-fn hello_world() {
-    println!("hello_world")
 }
 
 fn main() {
