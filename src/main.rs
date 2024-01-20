759e367e734b27c5b9ae7f60739292a1fe026f02
Tocha Mateusz <tocha.mateusz+main@gmail.com>
2024-01-20 19:16:29 +0100

    feat: Basic components in bevy

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Test1".to_string())));
+    commands.spawn((Person, Name("Test2".to_string())));
+    commands.spawn((Person, Name("Test2".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Test1" {
+            name.0 = "Test Updated".to_string();
+            break;
+        }
+    }
+}
+
+fn hello_world() {
+    println!("hello_world")
+}
+
+fn main() {
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .run()
+}
